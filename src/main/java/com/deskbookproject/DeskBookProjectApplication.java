4f7cd740d34084453a61ce838da7f1ff51c795a3
Preet0505 <preetpatel5543@gmail.com>
2023-05-15 23:32:11 +0530

    my first commit

---
+++ src/main/java/com/deskbookproject/DeskBookProjectApplication.java
@@ -0,0 +1,13 @@
+package com.deskbookproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskBookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskBookProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deskbookproject/DeskBookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deskbookproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskBookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
